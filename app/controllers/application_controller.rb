4671d4d568d2823b6fd0c495f1ea0e9ca5605a1e
Adam Kabak <adamkabak@yahoo.com>
2022-11-13 14:57:41 -0500

    commit tree 5e6262cdef0531728f251840e33c620c1131d866

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+  get '/' do
+    "<h1>Here we go!</h1>"
+  end
 
+  get '/bakeries' do
+    Bakery.all.to_json
+  end
+
+  get '/bakeries/:id' do
+    Bakery.find(params[:id]).to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    BakedGood.all.order(price: :desc).to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    BakedGood.all.order(price: :desc).first.to_json
+  end
 end
